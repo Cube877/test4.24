5d5c4b6533f760c6b52eff7c2172d80c124e5e29
Cube <877211593@qq.com>
2020-04-24 18:59:11 +0800

    wu

--- test4.24/test4.24.cpp
+++ test4.24/test4.24.cpp
@@ -2,12 +2,80 @@
 //
 
 #include <iostream>
+#include<opencv.hpp>
+#include <cmath>
+
+using namespace cv;
+using namespace std;
+
+void getHist(Mat srcMat, float * hist, int nX, int nY, int cellsize);
+float getDistance(float * hist1, float * hist2, int bins);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Mat refMat = imread("hogTemplate.jpg", 0);
+	Mat Mat1 = imread("img1.jpg", 0);
+	Mat Mat2 = imread("img2.jpg", 0);
+	int cellsize = 16;
+	int nX = refMat.cols / cellsize;
+	int nY = refMat.rows / cellsize;
+	int bins = 8 * nX * nY;
+	float * ref_hist = new float[bins];
+	memset(ref_hist, 0, sizeof(float)*bins);
+	float * hist1 = new float[bins];
+	memset(hist1, 0, sizeof(float)*bins);
+	float * hist2 = new float[bins];
+	memset(hist2, 0, sizeof(float)*bins);
+
+	getHist(refMat, ref_hist, nX, nY, cellsize);
+	getHist(Mat1, hist1, nX, nY, cellsize);
+	getHist(Mat2, hist2, nX, nY, cellsize);
+	float dis1 = getDistance(ref_hist, hist1, bins);
+	float dis2 = getDistance(ref_hist, hist2, bins);
+	cout << "与img1距离：" << dis1 << endl;
+	cout << "与img2距离：" << dis2 << endl;
+	if (dis1 < dis2)
+	{
+		cout << "与refMat更相似的是：img1" << endl;
+	}
+	else if (dis1 > dis2)
+	{
+		cout << "与refMat更相似的是：img2" << endl;
+	}
+	else
+	{
+		cout << "相似度相同" << endl;
+	}
+	delete[] ref_hist, hist1, hist2;
+	waitKey();
 }
 
+void getHist(Mat srcMat, float * hist, int nX, int nY, int cellsize)
+{
+	Mat gx, gy;
+	Mat mag, angle;
+	Sobel(srcMat, gx, CV_32F, 1, 0, 1);
+	Sobel(srcMat, gy, CV_32F, 0, 1, 1);
+	cartToPolar(gx, gy, mag, angle, true);
+	for (int i = 0; i < nY * 16; i++)
+	{
+		for (int j = 0; j < nX * 16; j++)
+		{
+			int angle_q = angle.at<float>(i, j) / 45;
+			*(hist + (j / cellsize * nX + i / cellsize) * 8 + angle_q) += mag.at<float>(i, j);
+		}
+	}
+}
+
+float getDistance(float * hist1, float * hist2, int bins)
+{
+	float sum = 0;
+	for (int i = 0; i < bins; i++)
+	{
+		sum += pow(*(hist1 + i) - *(hist2 + i), 2);
+	}
+	return sqrt(sum);
+}
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
 
